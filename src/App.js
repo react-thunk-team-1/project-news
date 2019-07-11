5b22060147c226a0828be51779455efa36fe732f
aditilham <aditilham152@gmail.com>
2019-07-11 15:23:29 +0700

    adding router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import CategoriesField from './components/CategoriesField'
 import TopNews from './containers/TopNews'
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+import Login from '';
+
 
 const App = () => (
+  <BrowserRouter>
   <div>
     <CategoriesField />
     <div id="empty-space"></div>
     <TopNews />
-  </div>
+  </div></BrowserRouter>
 )
 
 export default App;
