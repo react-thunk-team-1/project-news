a6a938ea2219e9d232c3e6a35759209c0f71ce15
Krishna Rowter <krishna.rowter@gmail.com>
2019-07-14 17:42:09 +0700

    add grid layout

--- src/containers/TopNews.js
+++ src/containers/TopNews.js
@@ -7,7 +7,7 @@ let TopNews = ({ categories, loading }) => {
 
   if (categories) {
     topNews = categories.map((article, index) => (
-      <div className="news-content" key={`${index}`}>
+      <div className="news-content grid-item" key={`${index}`}>
         <NewsItem article={categories[index]} />
       </div>
     ));
@@ -16,7 +16,7 @@ let TopNews = ({ categories, loading }) => {
     topNews = <h3 className="loading-indicator">Loading ...</h3>;
   }
 
-  return <div id="content-wrapper">{topNews}</div>;
+  return <div id="content-wrapper" class="grid" data-masonry='{ "itemSelector": ".grid-item", "columnWidth": 200 }'>{topNews}</div>;
 };
 
 const mapStateToProps = state => {
